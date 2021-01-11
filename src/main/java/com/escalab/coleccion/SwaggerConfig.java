0c1ad6ad3a543baf99051ebd9bcb57ba310de191
soRodriguezz <s.rodriguezzapata01@gmail.com>
2021-01-11 20:45:01 -0300

    edit swagger

--- src/main/java/com/escalab/coleccion/SwaggerConfig.java
+++ src/main/java/com/escalab/coleccion/SwaggerConfig.java
@@ -16,8 +16,8 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class SwaggerConfig {
 	public static final Contact DEFAULT_CONTACT = new Contact("Escalab", "https://escalab.academy",
-            "josse@escalab.academedy");
-    public static final ApiInfo DEFAULT_API_INFO = new ApiInfo("Mediapp Api Documentation", "Mediapp Api Documentation", "1.0",
+            "s.rodriguezzapata01@gmail.com");
+    public static final ApiInfo DEFAULT_API_INFO = new ApiInfo("Documentacion Coleccion", "Documento Coleccion", "1.0",
             "PREMIUM", DEFAULT_CONTACT, "Apache 2.0", "http://www.apache.org/licenses/LICENSE-2.0",
             new ArrayList<VendorExtension>());
     @Bean
